e5a2433af9dfd6d0146927c3cbfe61085fcf42ff
M Talha Sarif <mtalhasharif.paru05@gmal.com>
2024-02-06 19:33:01 +0500

    Add users name model or some modifications in nav-bar and edit form or also givs some links

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js"
 pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.5.1
+pin "popper", to: 'popper.js', preload: true
